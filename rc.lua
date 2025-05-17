821454ffd10e50c33d85e4a60b10c3265e1e1417
keyopto <keyopto@gmail.com>
2025-05-17 20:34:03 +0200

    feat: add bluetooth

--- rc.lua
+++ rc.lua
@@ -10,29 +10,29 @@ local menubar = require("menubar")
 require("awful.hotkeys_popup.keys")
 
 if awesome.startup_errors then
-	naughty.notify({
-		preset = naughty.config.presets.critical,
-		title = "Oops, there were errors during startup!",
-		text = awesome.startup_errors,
-	})
+  naughty.notify({
+    preset = naughty.config.presets.critical,
+    title = "Oops, there were errors during startup!",
+    text = awesome.startup_errors,
+  })
 end
 
 do
-	local in_error = false
-	awesome.connect_signal("debug::error", function(err)
-		-- Make sure we don't go into an endless error loop
-		if in_error then
-			return
-		end
-		in_error = true
+  local in_error = false
+  awesome.connect_signal("debug::error", function(err)
+    -- Make sure we don't go into an endless error loop
+    if in_error then
+      return
+    end
+    in_error = true
 
-		naughty.notify({
-			preset = naughty.config.presets.critical,
-			title = "Oops, an error happened!",
-			text = tostring(err),
-		})
-		in_error = false
-	end)
+    naughty.notify({
+      preset = naughty.config.presets.critical,
+      title = "Oops, an error happened!",
+      text = tostring(err),
+    })
+    in_error = false
+  end)
 end
 
 beautiful.init("~/.config/awesome/theme/theme.lua")
@@ -62,3 +62,4 @@ beautiful.useless_gap = 7
 awful.spawn.with_shell("picom -b")
 awful.spawn.with_shell("flameshot")
 awful.spawn.with_shell("nm-applet")
+awful.spawn.with_shell("blueman-applet")
